6ee3034dc0dba1ba861d00dcdf416b16e2cca7f0
Grijesh Chauhan <grijesh.mnit@gmail.com>
2019-04-12 13:46:30 +0530

    mysql to print type of triangles

---
+++ SQLs/TriangleTypes/triangletypes.sql
@@ -0,0 +1,15 @@
+select
+    case
+        when (a + b > c) and (b + c > a) and (c + a > b) then
+            case
+                when (a = b) and (a = c) then
+                    'Equilateral'
+                when (a = b) or (b = c) or (c = a) then
+                    'Isosceles'
+                else
+                    'Scalene'
+            end
+        else
+            'Not A Triangle'
+    end as type
+from triangles;
